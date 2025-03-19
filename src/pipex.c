7a424b202b08b419c144f69a513e32c4c1dde6d8
Daniel Castillo Correa <dacastil@c2r4s5.42madrid.com>
2025-03-19 14:05:47 +0100

    progress_80%_try_exec_command_failed

--- src/pipex.c
+++ src/pipex.c
@@ -6,34 +6,49 @@
 /*   By: dacastil <dacastil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/15 18:10:22 by dacastil          #+#    #+#             */
-/*   Updated: 2025/03/18 18:22:09 by dacastil         ###   ########.fr       */
+/*   Updated: 2025/03/19 14:00:33 by dacastil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/pipex.h"
 
-void	red_output(t_pid process)
+void	red_output(char *outfile)
 {
 	int	fd;
 
-	fd = open("../outfile.txt", O_WRONLY, 0644);
+	fd = open(outfile, O_WRONLY, 0644);
 	if (fd < 0)
 	{
-		fd = open("../outfile.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+		fd = open(outfile, O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	}
 	dup2(fd, STDOUT_FILENO);
 	close(fd);
 }
 
-void	red_input(t_pid process)
+void	red_input(char *infile)
 {
 	int	fd;
 
-	fd = open("../input_file.txt", O_RDONLY, 0664);
+	fd = open(infile, O_RDONLY, 0664);
 	if (fd < 0)
 		perror("Open failed\n");
 	dup2(fd, STDIN_FILENO);
 	close(fd);
+}
+
+void	process_command(char **envp, char **argv, int ix_argv)
+{
+	char	**rute_paths;
+	char	**command_arg;
+
+	rute_paths = ft_findpath(envp);
+	command_arg = split_command(argv[ix_argv]);
+	// for (int i = 0; rute_paths[i]; i++)
+	// {
+	// 	printf("paths ---> %s\n", rute_paths[i]);
+	// 	printf("paths ---> %s\n\n", command_arg[i]);
+	// }
+	exec_command(rute_paths, command_arg, argv, ix_argv);
 }
 
 void	create_process(t_pid process, char **argv, char **envp)
@@ -43,22 +58,22 @@ void	create_process(t_pid process, char **argv, char **envp)
 		perror("Fork failed\n");
 	else if (process.pid1 == 0)
 	{
-		red_input(process);
+		red_input(argv[1]);
 		close(process.pipefd[0]);
 		dup2(process.pipefd[1], STDOUT_FILENO);
 		close(process.pipefd[1]);
-		execve(ft_findpath(), separate(argv), separateenvp(envp))
+		process_command(envp, argv, 2);
 	}
 	process.pid2 = fork();
 	if (process.pid2 < 0)
 		perror("Fork failed\n");
 	else if (process.pid2 == 0)
 	{
-		red_output(process);
+		red_output(argv[4]);
 		close(process.pipefd[1]);
 		dup2(process.pipefd[0], STDIN_FILENO);
 		close(process.pipefd[0]);
-		execlp("wc", "wc", "-l", NULL);
+		process_command(envp, argv, 3);
 	}
 	close(process.pipefd[0]);
 	close(process.pipefd[1]);
@@ -68,7 +83,14 @@ void	create_process(t_pid process, char **argv, char **envp)
 int	main(int argc, char **argv, char **envp)
 {
 	t_pid	process; // proceso 1
+	// int	i;
 
+	// i = 0;
+	// while (envp[i])
+	// {
+	// 	printf("linea x = %s\n", envp[i]);
+	// 	i++;
+	// }
 	if ((argc - 1) != 4)
 		perror("Invalid Arguments\n");
 	if ((pipe(process.pipefd)) < 0)
--- src/utils_pipex.c
+++ src/utils_pipex.c
@@ -6,10 +6,99 @@
 /*   By: dacastil <dacastil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/18 18:16:28 by dacastil          #+#    #+#             */
-/*   Updated: 2025/03/18 18:18:41 by dacastil         ###   ########.fr       */
+/*   Updated: 2025/03/19 13:53:12 by dacastil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/pipex.h"
+#include "../libft_bonus/libft.h"
 
-void
+void	fr_words(char **wds)
+{
+	int	i;
+
+	i = 0;
+	while (wds[i])
+	{
+		free(wds[i]);
+		i++;
+	}
+	free(wds);
+}
+
+char	**ft_findpath(char **envp)
+{
+	int		i;
+	char	*line;
+	char	**wd;
+
+	i = 0;
+	while (envp[i])
+	{
+		if (ft_strncmp(envp[i], "PATH=", 5))
+			line = (envp[i] + 5); // retorna el string desde la posicion 5
+		i++;
+	}
+
+	wd = ft_split(line, ':');
+	return (wd);
+}
+
+char	**split_command(char *arg)
+{
+	char	**command_mask;
+	char	*command;
+	char	*str;
+
+	str = ft_strchr(arg, '\'');
+	if (!str) // si no tiene 'arg'
+	{
+		command_mask = ft_split(arg, ' ');
+		free(arg);
+		return (command_mask);
+	}
+	else
+	{
+		command_mask = ft_split(arg, ' ');
+		command = command_mask[0];
+		fr_words(command_mask);
+		command_mask = (char *[]){command, ft_strtrim(str, "'"), NULL};
+		free(str);
+		free(command);
+		return (command_mask);
+	}
+}
+char	*creat_path(char	*path, char *argv)
+{
+	char	*result;
+	char	*base_temp;
+
+	if (ft_strchr(argv, '/')) // Por si viene con ruta
+		return (ft_strdup(argv));
+	base_temp = ft_strjoin(path, "/"); // base de la ruta
+	result = ft_strjoin(base_temp, argv); // la ruta mas el comando, ruta completa
+	if (!result)
+		return (NULL);
+	free(base_temp);
+	return(result);
+}
+
+
+void	exec_command(char **rute_paths, char **command_arg, char **argv, int ix)
+{
+	char	*path;
+	int		i;
+
+	i = 0;
+	while (rute_paths[i])
+	{
+		path = creat_path(rute_paths[i], argv[ix]);
+		if (access(path, X_OK) == 0)
+		{
+			execve(path, command_arg, rute_paths);
+		}
+		free(path);
+		i++;
+	}
+
+}
